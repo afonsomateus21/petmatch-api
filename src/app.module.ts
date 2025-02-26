21fe445f193932741dc1e86137f9edada0e5220c
afonsomateus21 <afonsomateus.dev@gmail.com>
2025-02-25 23:45:32 -0300

    feat: cria modulo adopter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnimalModule } from './animal/animal.module';
+import { AdopterModule } from './adopter/adopter.module';
 
 @Module({
-  imports: [AnimalModule],
+  imports: [AnimalModule, AdopterModule],
   controllers: [AppController],
   providers: [AppService],
 })
